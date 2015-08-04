3c574577c0946b7deff29338d919dfc1329aa3fb
J.Noe Hernandez V <donoe1985@gmail.com>
2015-08-03 20:18:08 -0500

    First Commit

---
+++ soccer/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Team(models.Model):
+    name = models.CharField(max_length=200)
+
+    def __unicode__(self):
+        return self.name
+
+class Player(models.Model):
+    name = models.CharField(max_length=200)
+    team = models.ForeignKey(Team)
+
+    def __unicode__(self):
+        return u"{name} {team}".format(name=self.name, team=self.team.name)
