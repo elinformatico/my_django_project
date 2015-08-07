9259baed1949729d25cabf60531168e105199dbf
J.Noe Hernandez V <donoe1985@gmail.com>
2015-08-06 19:47:44 -0500

    Change the database

--- django_project/settings.py
+++ django_project/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'django',
         'USER': 'django',
-        'PASSWORD': 'MAIEB3eJol',
+        'PASSWORD': 'nC1VsZOHMw',
         'HOST': 'localhost',
         'PORT': '',
     }
